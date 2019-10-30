150a845f38afbcb0ab26f1cbb3cd225cac59da3f
Md Rifat Arefin <rifat.arefin515@gmail.com>
2019-10-31 00:05:32 +0600

    Initial commit

---
+++ src/DataLoader.py
@@ -0,0 +1,94 @@
+from PIL import Image
+from torch.utils.data import Dataset
+from os.path import join
+import numpy as np
+import h5py
+import glob
+
+
+def selectRandomSample(bounding_infos, n):
+    length = len(bounding_infos)
+
+    while True:
+        i = np.random.randint(low=0, high=length)
+
+        item = bounding_infos[i]
+        num_digits = len(item['label'])
+
+        if num_digits <= n:
+            return i, item, num_digits
+
+
+def getBoundingBoxAroundAllDigits(item):
+    attrs_left, attrs_top, attrs_width, attrs_height = map(lambda x: [int(i) for i in x],
+                                                           [item['left'], item['top'], item['width'],
+                                                            item['height']])
+    min_left, min_top, max_right, max_bottom = (min(attrs_left),
+                                                min(attrs_top),
+                                                max(map(lambda x, y: x + y, attrs_left, attrs_width)),
+                                                max(map(lambda x, y: x + y, attrs_top, attrs_height)))
+    center_x, center_y, max_side = ((min_left + max_right) / 2.0,
+                                    (min_top + max_bottom) / 2.0,
+                                    max(max_right - min_left, max_bottom - min_top))
+    bbox_left, bbox_top, bbox_width, bbox_height = (center_x - max_side / 2.0,
+                                                    center_y - max_side / 2.0,
+                                                    max_side,
+                                                    max_side)
+
+    cropped_left, cropped_top, cropped_width, cropped_height = (int(round(bbox_left - 0.15 * bbox_width)),
+                                                                int(round(bbox_top - 0.15 * bbox_height)),
+                                                                int(round(bbox_width * 1.3)),
+                                                                int(round(bbox_height * 1.3)))
+
+    return cropped_left, cropped_top, cropped_width, cropped_height
+
+
+def getBBInfo(file1, index):
+    attrs = {}
+    file = file1
+    item = file['digitStruct']['bbox'][index].item()
+    for key in ['label', 'left', 'top', 'width', 'height']:
+        attr = file[item][key]
+        values = [file[attr[i].item()][0][0] for i in range(len(attr))] if len(attr) > 1 else [attr[0][0]]
+        attrs[key] = values
+
+    return attrs
+
+
+class DataSet(Dataset):
+    def __init__(self, data_dir, digit_struct_mat_file, transform=None):
+        super(DataSet, self).__init__()
+        self.data_dir = data_dir
+        self.img_names = glob.glob(join(data_dir, '*.png'))
+        self.f = h5py.File(digit_struct_mat_file, 'r')
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_names)
+
+    def __getitem__(self, index):
+
+        item = getBBInfo(self.f, index)
+        num_digits = len(item['label'])
+
+        if num_digits > 5:
+            # number of digits are greater than n=5 in an image, we will select another image randomly
+            index, item, num_digits = selectRandomSample(self.bounding_infos, n=5)
+
+        left, top, width, height = getBoundingBoxAroundAllDigits(item)
+
+        path = join(self.data_dir, str(index) + '.png')
+        img = Image.open(path)
+        img = img.crop([left, top, left + width, top + height])
+        img = img.resize([64, 64])
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        label_of_digits = item['label']
+        digits = [10, 10, 10, 10, 10]  # we look for maximum n=5 digits in an image, digit 10 represents no digit
+        for i, label_of_digit in enumerate(label_of_digits):
+            digits[i] = int(label_of_digit if label_of_digit != 10 else 0)
+
+
+        return img, num_digits, digits
---
+++ src/Main.py
@@ -0,0 +1,175 @@
+from DataLoader import DataSet
+from torchvision import transforms
+import json
+import torch
+import numpy as np
+from Model import DigitNet
+import torch.optim as optim
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
+from os.path import join
+import os
+import datetime
+from tensorboardX import SummaryWriter
+
+
+def get_loss(length_logit, digit_logits, length_labels, digits_labels):
+
+    loss = torch.nn.functional.cross_entropy(length_logit, length_labels)
+
+    for i in range(len(digit_logits)):
+        loss += torch.nn.functional.cross_entropy(digit_logits[i], digits_labels[i])
+
+    return loss
+
+
+def calculate_correct(digit_logits, digits_labels):
+
+    digit_prediction = []
+    for i in range(len(digit_logits)):
+        digit_prediction.append(digit_logits[i].max(1)[1])
+
+    num_correct = 0
+    num_correct += (digit_prediction[0].eq(digits_labels[0]) &
+                    digit_prediction[1].eq(digits_labels[1]) &
+                    digit_prediction[2].eq(digits_labels[2]) &
+                    digit_prediction[3].eq(digits_labels[3]) &
+                    digit_prediction[4].eq(digits_labels[4])).sum()
+
+    return num_correct
+
+
+def trainAndGetBestModel(model, optimizer, dataloaders, config):
+    np.random.seed(11)  # seed all RNGs for reproducibility
+    torch.manual_seed(11)
+
+    num_epochs = config["training"]["num_epochs"]
+    batch_size = config["training"]["batch_size"]
+
+    subfolder_pattern = 'batch_{}_time_{}'.format(batch_size, f"{datetime.datetime.now():%Y-%m-%d-%H-%M-%S-%f}")
+
+    checkpoint_dir = os.path.join(config["paths"]["checkpoint_dir"], 'checkpoints', subfolder_pattern)
+    os.makedirs(checkpoint_dir, exist_ok=True)
+
+    logging_dir = os.path.join(config['paths']['tb_log_file_dir'], 'log', subfolder_pattern)
+    os.makedirs(logging_dir, exist_ok=True)
+
+    writer = SummaryWriter(logging_dir)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model.to(device)
+
+    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=config['training']['lr_decay'],
+                                               verbose=True, patience=config['training']['lr_step'])
+
+    best_acc = 0
+    for epoch in range(1, num_epochs + 1):
+
+        # Train
+        model.train()
+        train_loss, train_acc = 0.0,  0.0  # monitor train loss and accuracy
+
+        # Iterate over data
+        for img, length_labels, digit_labels in dataloaders['train']:
+
+            optimizer.zero_grad()  # zero the parameter gradients
+
+            img = img.float().to(device)
+            length_labels = length_labels.to(device)
+            digit_labels = [digit.to(device) for digit in digit_labels]
+
+            length_logit, digit_logits = model(img)
+
+            loss = get_loss(length_logit, digit_logits, length_labels, digit_labels)
+
+            # Backprop
+            loss.backward()
+            optimizer.step()
+            epoch_loss = loss.detach().cpu().numpy() * len(img) / len(dataloaders['train'].dataset)
+            train_loss += epoch_loss
+            train_acc += calculate_correct(digit_logits, digit_labels)
+
+
+        # Evaluation
+        model.eval()
+        val_loss, val_acc = 0.0,  0.0  # monitor val loss and accuracy
+
+        for img, length_labels, digit_labels in dataloaders['val']:
+            img = img.float().to(device)
+            length_labels = length_labels.to(device)
+            digit_labels = [digit.to(device) for digit in digit_labels]
+
+            length_logit, digit_logits = model(img)
+
+            loss = get_loss(length_logit, digit_logits, length_labels, digit_labels)
+
+            epoch_loss = loss.detach().cpu().numpy() * len(img) / len(dataloaders['val'].dataset)
+            val_loss += epoch_loss
+            val_acc += calculate_correct(digit_logits, digit_labels)
+
+        train_loss /= len(dataloaders['train'].dataset)
+        val_loss /= len(dataloaders['val'].dataset)
+
+        train_acc /= len(dataloaders['train'].dataset)
+        val_acc /= len(dataloaders['val'].dataset)
+
+        train_acc /= len(dataloaders['train'].dataset)
+        val_acc /= len(dataloaders['val'].dataset)
+
+        if best_acc > val_acc:
+            torch.save(model.state_dict(), os.path.join(checkpoint_dir, 'DigitNet.pth'))
+            best_acc = val_acc
+
+        writer.add_scalar("train/loss", train_loss, epoch)
+        writer.add_scalar("train/accuracy", train_acc, epoch)
+        writer.add_scalar("val/loss", val_loss, epoch)
+        writer.add_scalar("val/accuracy", val_acc, epoch)
+        scheduler.step(val_acc)
+    writer.close()
+
+
+def main(config):
+
+    # Reproducibility options
+    np.random.seed(0)  # RNG seeds
+    torch.manual_seed(0)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    # Initialize the network based on the network configuration
+    model = DigitNet()
+
+    optimizer = optim.Adam(model.parameters(), lr=config["training"]["lr"])
+
+    data_directory = config["paths"]["data_directory"]
+    batch_size = config["training"]["batch_size"]
+    n_workers = config["training"]["n_workers"]
+
+    transform = transforms.Compose([
+        transforms.RandomCrop([54, 54]),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+    train_dataset = DataSet(join(data_directory, "train"), join(data_directory, "train", "digitStruct.mat"), transform)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=n_workers, pin_memory=True)
+
+    val_dataset = DataSet(join(data_directory, "extra"), join(data_directory, "extra", "digitStruct.mat"), transform)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=n_workers, pin_memory=True)
+
+    dataloaders = {'train': train_dataloader, 'val': val_dataloader}
+
+    # Train model
+    torch.cuda.empty_cache()
+
+    trainAndGetBestModel(model, optimizer, dataloaders, config)
+
+
+if __name__ == '__main__':
+
+    with open('config.json', "r") as read_file:
+        config = json.load(read_file)
+
+    main(config)
