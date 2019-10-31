4cbaad7b0feae391ab63063bf5b61cee5a32f37a
Md Rifat Arefin <rifat.arefin515@gmail.com>
2019-10-31 22:18:58 +0600

    model changed

--- src/Model.py
+++ src/Model.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-import torch
+
 
 class Block(nn.Module):
     def __init__(self, in_channels, out_channels, conv_kernel, pool_kernel, pool_stride):
@@ -23,33 +23,35 @@ class DigitNet(nn.Module):
         super(DigitNet, self).__init__()
 
         self.features = nn.Sequential(
-            Block(in_channels=3, out_channels=32, conv_kernel=3, pool_kernel=2, pool_stride=2),
-            Block(in_channels=32, out_channels=64, conv_kernel=3, pool_kernel=2, pool_stride=1),
-            Block(in_channels=64, out_channels=128, conv_kernel=3, pool_kernel=2, pool_stride=2),
-            Block(in_channels=128, out_channels=192, conv_kernel=3, pool_kernel=2, pool_stride=1),
-            Block(in_channels=192, out_channels=192, conv_kernel=3, pool_kernel=2, pool_stride=2),
-            Block(in_channels=192, out_channels=192, conv_kernel=3, pool_kernel=2, pool_stride=2),
-            # Block(in_channels=192, out_channels=192, conv_kernel=5, pool_kernel=2, pool_stride=2),
-            # Block(in_channels=192, out_channels=192, conv_kernel=5, pool_kernel=2, pool_stride=1)
+            Block(in_channels=3, out_channels=48, conv_kernel=5, pool_kernel=2, pool_stride=2),
+            Block(in_channels=48, out_channels=64, conv_kernel=5, pool_kernel=2, pool_stride=1),
+            Block(in_channels=64, out_channels=128, conv_kernel=5, pool_kernel=2, pool_stride=2),
+            Block(in_channels=128, out_channels=160, conv_kernel=5, pool_kernel=2, pool_stride=1),
+            Block(in_channels=160, out_channels=192, conv_kernel=5, pool_kernel=2, pool_stride=2),
+            Block(in_channels=192, out_channels=192, conv_kernel=5, pool_kernel=2, pool_stride=1),
+            Block(in_channels=192, out_channels=192, conv_kernel=5, pool_kernel=2, pool_stride=2),
+            Block(in_channels=192, out_channels=192, conv_kernel=5, pool_kernel=2, pool_stride=1)
         )
 
         self.fully_connected = nn.Sequential(
-            nn.Linear(192 * 5 * 5, 4096),
+            nn.Linear(192 * 7 * 7, 3072),
+            nn.PReLU(0.5),
+            nn.Dropout(),
+            nn.Linear(3072, 3072),
             nn.PReLU(),
-            nn.Linear(4096, 4096),
-            nn.PReLU()
+            nn.Dropout(0.5)
         )
 
-        self.digit_length = nn.Sequential(nn.Linear(4096, 7))
-        self.digit1 = nn.Sequential(nn.Linear(4096, 11))
-        self.digit2 = nn.Sequential(nn.Linear(4096, 11))
-        self.digit3 = nn.Sequential(nn.Linear(4096, 11))
-        self.digit4 = nn.Sequential(nn.Linear(4096, 11))
-        self.digit5 = nn.Sequential(nn.Linear(4096, 11))
+        self.digit_length = nn.Sequential(nn.Linear(3072, 7))
+        self.digit1 = nn.Sequential(nn.Linear(3072, 11))
+        self.digit2 = nn.Sequential(nn.Linear(3072, 11))
+        self.digit3 = nn.Sequential(nn.Linear(3072, 11))
+        self.digit4 = nn.Sequential(nn.Linear(3072, 11))
+        self.digit5 = nn.Sequential(nn.Linear(3072, 11))
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), 192 * 5 * 5)
+        x = x.view(x.size(0), 192 * 7 * 7)
         x = self.fully_connected(x)
 
         length_logits = self.digit_length(x)
@@ -60,9 +62,3 @@ class DigitNet(nn.Module):
         digit5_logits = self.digit5(x)
 
         return length_logits, [digit1_logits, digit2_logits, digit3_logits, digit4_logits, digit5_logits]
-#
-# model = DigitNet()
-# x = torch.rand(8, 3, 54, 54)
-# y = model(x)
-#
-# print(y.shape)
